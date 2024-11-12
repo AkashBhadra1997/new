4b49deb6b2a99f863a356d41b29505c74b4be7bc
Akashbhadra1997 <bhadra.akash97@gmail.com>
2024-11-12 18:07:15 +0530

    this is my first commit

---
+++ test.py
@@ -0,0 +1,38 @@
+# it = [1,23,45,5,67,56,3]
+
+l = []
+# def assending_order(l):
+#     l1 = []
+#     for i in range(len(l)):
+#         c = 0
+#         for j in l:
+#             if l[i] >= j:
+#                 c += 1
+#         l1.insert(c-1,l[i])
+#     return l1
+#
+# print(assending_order(it))
+
+# item = [1,23,45,5,67,56,3]
+
+# for i in range(len(item)):
+#     for j in range(0,len(item)-1-i):
+#         if item[j] > item[j+1]:
+#             item[j],item[j+1] = item[j+1],item[j]
+#
+# print(item)
+# for i in range(len(item)):
+#     for j in range(0,len(item)-1-i):
+#         if item[j] > item[j+1]:
+#             item[j] = item[j+1]
+#             item[j+1] = item[j]
+
+# from selenium.webdriver.chrome.webdriver import WebDriver
+#
+# driver = WebDriver()
+#
+# driver.get("file:///C:/Users/akash/Downloads/demo.html")
+# row_count = driver.find_elements("xpath",'//table[@name="os"]//tr')
+# print(len(row_count))
+
+
